e4e2a1e5490dccc740e59b74cf3139730d2de78a
MoonHyunSeong <gustjd617@gmail.com>
2022-07-06 14:07:18 +0900

    싱글톤 패턴
    싱글톤 사용 시 주의할 점
    configuration 의 마법 등
    
    스프링이 해주는게 많구나~ 감동이야

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -21,4 +21,9 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository.findById(memberId);
     }
 
+
+    //test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
